4161b7ba47f0061ad0cc118fe83696654d59fff8
rafael-almeida <rafaelalmeida37@ufrrj.br>
2024-10-14 18:04:07 -0300

    Criacao das rotas e upload de imagens

--- app/controllers/products_controller.ts
+++ app/controllers/products_controller.ts
@@ -4,6 +4,29 @@ import { cuid } from '@adonisjs/core/helpers'
 import app from '@adonisjs/core/services/app'
 
 export default class ProductsController {
+  async index() {
+    const allProducts = await Product.all()
+
+    return allProducts
+  }
+
+  async findByType({ request }: HttpContext) {
+    const page = request.input('page', 1)
+    const limit = 10
+
+    const payload = request.only(['type'])
+
+    const query = Product.query()
+
+    if (payload.type && payload.type.length > 0) {
+      query.where('type', 'like', `%${payload.type}%`)
+    }
+
+    const products = await query.paginate(page, limit)
+
+    return products
+  }
+
   async create({ request, response }: HttpContext) {
     const imagePost = request.file('image', {
       size: '20mb',
@@ -30,5 +53,55 @@ export default class ProductsController {
     await product.save()
 
     return response.created(product)
+  }
+
+  async show({ params }: HttpContext) {
+    const product = await Product.findOrFail(params.id)
+
+    return product
+  }
+
+  async delete({ params }: HttpContext) {
+    const productIdToDelete = await Product.findOrFail(params.id)
+
+    await productIdToDelete.delete()
+
+    return { sucess: `${params.id} removido` }
+  }
+
+  async update({ params, request, response }: HttpContext) {
+    const productToUpdate = await Product.findOrFail(params.id)
+
+    const fields = request.only(['name', 'price', 'type', 'description'])
+    productToUpdate.merge(fields)
+
+    const imagePost = request.file('image', {
+      size: '20mb',
+      extnames: ['jpg', 'png', 'jpeg'],
+    })
+
+    if (imagePost) {
+      const uploadPath = 'storage/uploads'
+      await imagePost.move(app.makePath(uploadPath), {
+        name: `${cuid()}.${imagePost.extname}`,
+      })
+      productToUpdate.image_path = `${uploadPath}/${imagePost.fileName}`
+    }
+
+    await productToUpdate.save()
+
+    return response.ok(productToUpdate)
+  }
+
+  async showImage({ params, response }: HttpContext) {
+    try {
+      const product = await Product.findOrFail(params.id)
+
+      const imagePath = product.image_path
+
+      return response.download(imagePath)
+    } catch (error) {
+      return response.notFound('Produto não encontrado ou imagem não disponível')
+    }
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,10 +1,17 @@
 import router from '@adonisjs/core/services/router'
-import { create } from 'domain'
 
 const ProductsController = () => import('#controllers/products_controller')
 
 router
   .group(() => {
+    router.get('/', [ProductsController, 'index'])
+    router.get('/:id', [ProductsController, 'show']).where('id', router.matchers.number())
+    router.get('/type', [ProductsController, 'findByType'])
+    router
+      .get('/image/:id', [ProductsController, 'showImage'])
+      .where('id', router.matchers.number())
     router.post('/add', [ProductsController, 'create'])
+    router.delete('/:id', [ProductsController, 'delete']).where('id', router.matchers.number())
+    router.put('/:id', [ProductsController, 'update']).where('id', router.matchers.number())
   })
   .prefix('products')
